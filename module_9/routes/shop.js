e6670f038060e5de2bf5872f6fb09d2247bf5600
Abhinav Tiwari <tiwariabhinavin@gmail.com>
2021-01-04 16:53:48 +0530

    done some advanced rputing stuff , edited the products model to include more parametes

--- module_9/routes/shop.js
+++ module_9/routes/shop.js
@@ -8,12 +8,14 @@ const router = express.Router();
 
 router.get("/", shopController.getIndex);
 
-router.get("/cart", shopController.getCart);
-
-router.get("/checkout", shopController.getCheckout);
-
 router.get("/products", shopController.getProducts);
 
+router.get("/cart", shopController.getCart);
+
 router.get("/orders", shopController.getOrders);
+
+router.get("/products/:productId", shopController.getProduct);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
